7589ff70a621c896ac0cfb98fdec475d5a64d8ec
realvjy <realvjy@gmail.com>
2020-12-05 20:35:23 +0530

    issue 22

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,9 +78,9 @@ const BlogIndex = ({ data, location }: PageProps<Data>) => {
               </div>
           </div>
           <div className="latest-issue">
-            <a href="https://bit.ly/designletter-21" className="issue-box">
-              <h2>#21 - Why we love Figma</h2>
-              <h5>Nov 25, 2020</h5>
+            <a href="https://bit.ly/designletter-22" className="issue-box">
+              <h2>#22 - Open Source Design</h2>
+              <h5>Dec 2, 2020</h5>
             </a>
             <p>Here's an archive of all the <a href="/archive">previous issues</a></p>
           </div>
